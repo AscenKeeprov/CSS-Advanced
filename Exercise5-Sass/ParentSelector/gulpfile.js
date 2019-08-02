d3ed83eb799f6e42569aaf762e95d97d74df7ddb
Ascen Keeprov <assenkiprov@yahoo.com>
2019-08-02 14:34:26 +0300

    Exercise5 upload

---
+++ Exercise5-Sass/ParentSelector/gulpfile.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const styleDestination = './';
+const styleSource = 'style.scss';
+
+gulpSass.compiler = require('node-sass');
+
+function loadStyles() {
+	return gulp.src(`${styleSource}`)
+		.pipe(gulpSass({
+			indentType: 'tab',
+			indentWidth: 1,
+			linefeed: 'crlf',
+			outputStyle: 'expanded'
+		}).on('error', gulpSass.logError))
+		.pipe(gulp.dest(`${styleDestination}`));
+}
+
+function watchForChanges() {
+	gulp.watch(`${styleSource}`, loadStyles);
+}
+
+exports.default = gulp.series(loadStyles, watchForChanges);
